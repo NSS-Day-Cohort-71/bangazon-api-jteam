da18bbc817b39138ff3c3f66e460292ee029193d
Jeremy Whitney <bearwhitness@gmail.com>
2024-09-24 15:18:34 -0500

    Created fixtures for new and updated models

--- seed_data.sh
+++ seed_data.sh
@@ -12,3 +12,12 @@ python manage.py loaddata product
 python manage.py loaddata payment
 python manage.py loaddata order
 python manage.py loaddata order_product
+python manage.py loaddata likes
+python manage.py loaddata ratings
+python manage.py loaddata recommendations
+python manage.py loaddata stores
+python manage.py loaddata storeproducts
+python manage.py loaddata favoritesellers
+python manage.py loaddata productrating
+
+
