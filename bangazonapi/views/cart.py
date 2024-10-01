d30a31f213ae5adee8b1a94b26a7ed34a2dfe553
Jana Ismail <jana.ismail.nss@gmail.com>
2024-10-01 09:20:57 -0500

    Refactored so Add to Cart functionality works

--- bangazonapi/views/cart.py
+++ bangazonapi/views/cart.py
@@ -7,11 +7,15 @@ from bangazonapi.models import Order, Customer, Product, OrderProduct
 from .product import ProductSerializer
 from .order import OrderSerializer
 
+# Need to create an OrderProductSerializer(serializer.ModelSerializer):
+    # class Meta:
+        # model = OrderProduct
+        # fields = // specify fields to include in response
 
 class Cart(ViewSet):
     """Shopping cart for Bangazon eCommerce"""
 
-    def create(self, request):
+    def create(self, request, pk=None):
         """
         @api {POST} /cart POST new line items to cart
         @apiName AddLineItem
@@ -32,10 +36,14 @@ class Cart(ViewSet):
             open_order.customer = current_user
             open_order.save()
 
+        product_id = request.data.get('product_id')
+        
         line_item = OrderProduct()
-        line_item.product = Product.objects.get(pk=request.data["product_id"])
+        line_item.product = Product.objects.get(pk=product_id)
         line_item.order = open_order
         line_item.save()
+
+        # use serializer for proper response.data format
 
         return Response({}, status=status.HTTP_204_NO_CONTENT)
 
