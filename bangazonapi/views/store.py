4a135824f22dddb6973ade2633691a94e954eb5e
Jeremy Whitney <bearwhitness@gmail.com>
2024-10-07 13:02:08 -0500

    Added a 'get_products' decorator to 'StoreSerializer' to add products sold by store to the 'store' response

--- bangazonapi/views/store.py
+++ bangazonapi/views/store.py
@@ -1,28 +1,37 @@
 from rest_framework import serializers, viewsets
 from django.contrib.auth.models import User
-from bangazonapi.models import Store, Customer
+from bangazonapi.models import Store, Customer, StoreProduct
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from .product import ProductSerializer
 
 
 class StoreOwnerSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("id", "first_name", "last_name",)
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+        )
+
 
 class StoreSerializer(serializers.ModelSerializer):
     """JSON serializer"""
 
     customer = StoreOwnerSerializer(source="customer.user", read_only=True)
+    products = serializers.SerializerMethodField()
+
+    def get_products(self, obj):
+        # Get all StoreProduct objects for this store
+        store_products = StoreProduct.objects.filter(store=obj)
+        # Extract the actual Product objects
+        products = [sp.product for sp in store_products]
+        return ProductSerializer(products, many=True, context={'request': self.context.get('request')}).data
 
     class Meta:
         model = Store
-        fields = (
-            "id",
-            "customer",
-            "name",
-            "description",
-        )
+        fields = ("id", "customer", "name", "description", "products")
         read_only_fields = ["customer"]
 
 
@@ -31,21 +40,26 @@ class StoreViewSet(viewsets.ModelViewSet):
     serializer_class = StoreSerializer
     pagination_class = None
 
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['request'] = self.request
+        return context
+
     def perform_create(self, serializer):
         # Fetch the logged-in user's related customer instance
         customer = Customer.objects.get(user=self.request.user)
         serializer.save(customer=customer)
 
-    @action(detail=True, methods=['post'])
+    @action(detail=True, methods=["post"])
     def favorite(self, request, pk=None):
         store = self.get_object()
         customer = Customer.objects.get(user=request.user)
         customer.add_favorite_store(store)
-        return Response({'status': 'store favorited'})
+        return Response({"status": "store favorited"})
 
-    @action(detail=True, methods=['post'])
+    @action(detail=True, methods=["post"])
     def unfavorite(self, request, pk=None):
         store = self.get_object()
         customer = Customer.objects.get(user=request.user)
         customer.remove_favorite_store(store)
-        return Response({'status': 'store unfavorited'})
+        return Response({"status": "store unfavorited"})
